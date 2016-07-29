529c3e5a8701b870340994c723193548ae779319
Randy <randy.zhang@youtile.com>
2016-07-29 17:23:00 +0800

    提交AppsExploreActivity

---
+++ app/src/main/java/com/rainmonth/activity/AppsExploreActivity.java
@@ -0,0 +1,34 @@
+package com.rainmonth.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+
+import com.rainmonth.R;
+import com.rainmonth.fragment.CardViewPagerFragment;
+
+public class AppsExploreActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean isApplyTranslucentStatusBar() {
+        return false;
+    }
+
+    @Override
+    public int getContentViewLayoutId() {
+        return R.layout.activity_apps_explore;
+    }
+
+    @Override
+    public void initViewsAndEvent() {
+        CardViewPagerFragment fragment = CardViewPagerFragment.getInstance();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frameLayout, fragment);
+        transaction.commit();
+    }
+}
