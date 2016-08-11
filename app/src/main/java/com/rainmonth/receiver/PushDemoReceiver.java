f517de6761dace93cfd3a4f6977bdf5d0bfdb25b
Randy <rainmonth@yeah.net>
2016-08-11 11:43:29 +0800

    通知更改

--- app/src/main/java/com/rainmonth/receiver/PushDemoReceiver.java
+++ app/src/main/java/com/rainmonth/receiver/PushDemoReceiver.java
@@ -1,13 +1,24 @@
 package com.rainmonth.receiver;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import com.igexin.sdk.PushConsts;
 import com.igexin.sdk.PushManager;
+import com.rainmonth.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PushDemoReceiver extends BroadcastReceiver {
 
@@ -15,6 +26,8 @@ public class PushDemoReceiver extends BroadcastReceiver {
      * 应用未启动, 个推 service已经被唤醒,保存在该时间段内离线消息(此时 GetuiSdkDemoActivity.tLogView == null)
      */
     public static StringBuilder payloadData = new StringBuilder();
+
+    private final String PUSH_NOTIFICATION_CLICK_ACTION = "com.rainmonth.action.PushNotificationClickAction";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -34,17 +47,11 @@ public class PushDemoReceiver extends BroadcastReceiver {
                 boolean result = PushManager.getInstance().sendFeedbackMessage(context, taskid, messageid, 90001);
                 System.out.println("第三方回执接口调用" + (result ? "成功" : "失败"));
 
+
                 if (payload != null) {
                     String data = new String(payload);
-
-                    Log.d("GetuiSdkDemo", "receiver payload : " + data);
-
-                    payloadData.append(data);
-                    payloadData.append("\n");
-
-//                    if (GetuiSdkDemoActivity.tLogView != null) {
-//                        GetuiSdkDemoActivity.tLogView.append(data + "\n");
-//                    }
+                    // todo 处理json数据
+                    dealWithMsgData(context, data);
                 }
                 break;
 
@@ -130,6 +137,110 @@ public class PushDemoReceiver extends BroadcastReceiver {
 
             default:
                 break;
+        }
+    }
+
+    private void dealWithMsgData(Context context, String message) {
+        String notifyTime = DateFormat.format("kk:mm", System.currentTimeMillis()).toString();
+        try {
+            JSONObject messageObj = new JSONObject(message);
+            // 数据获取
+            if (!messageObj.isNull("custom_content")) {
+                JSONObject ccObj = messageObj.getJSONObject("custom_content");
+
+                JSONObject keyObj = ccObj.getJSONObject("key");
+                String title = keyObj.getString("title");
+                String content = keyObj.getString("content");
+                String ctime = keyObj.getString("ctime");
+                String notice_type = !keyObj.isNull("notice_type") ? keyObj.getString("notice_type") : "";
+                String notice_id = !keyObj.isNull("id") ? keyObj.getString("id") : "";
+                String prj_id = !keyObj.isNull("prj_id") ? keyObj.getString("prj_id") : "";
+                String now_time = !keyObj.isNull("now_time") ? keyObj.getString("now_time") : "";
+                //IOS闹钟推送标识
+                if ("system_alarm_push".equals(notice_type)) {
+                    return;
+                }
+
+                //闹钟
+                if ("alarm".equals(notice_type)) {
+                    return;
+                }
+
+                if (!"activity".equals(notice_type)) {
+                    Intent intent = new Intent(PUSH_NOTIFICATION_CLICK_ACTION);
+                    Bundle bundle = new Bundle();
+                    bundle.putString("noticetype", notice_type);
+                    bundle.putString("content", content);
+                    bundle.putString("title", title);
+                    bundle.putString("datetime", ctime);
+                    bundle.putString("notice_id", notice_id);
+                    bundle.putString("prj_id", prj_id);
+                    intent.putExtras(bundle);
+
+                    createNotification(context, title, content, notifyTime, intent);
+                }
+                context.sendBroadcast(new Intent().setAction("MARK_HOOK_MARK_ACTION"));
+            }
+
+        } catch (JSONException e) {
+            Intent intent = new Intent(PUSH_NOTIFICATION_CLICK_ACTION);
+            Bundle bundle = new Bundle();
+            String title = context.getResources().getString(R.string.app_name);
+            bundle.putString("noticetype", "launchMain");
+            bundle.putString("content", message);
+            bundle.putString("title", title);
+//            bundle.putString("datetime", ctime);
+//            bundle.putString("notice_id", notice_id);
+//            bundle.putString("prj_id", prj_id);
+            intent.putExtras(bundle);
+            createNotification(context, title, message, notifyTime, intent);
+            e.printStackTrace();
+        }
+    }
+
+    private void createNotification(Context context, String title, String content, String notifyTime, Intent intent) {
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                context, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationManager nManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        // todo 待完善
+        RemoteViews smallRemoteView = new RemoteViews(context.getPackageName(), R.layout.notification_custom_builder);
+        smallRemoteView.setImageViewBitmap(R.id.notification_icon, BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher));
+        smallRemoteView.setTextViewText(R.id.notification_title, title);
+        smallRemoteView.setTextViewText(R.id.notification_text, content);
+        smallRemoteView.setTextViewText(R.id.notification_time, notifyTime);
+        // todo 待完善
+        RemoteViews bigRemoteView = new RemoteViews(context.getPackageName(), R.layout.notification_custom_builder);
+        bigRemoteView.setImageViewBitmap(R.id.notification_icon, BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher));
+        bigRemoteView.setTextViewText(R.id.notification_title, title);
+        bigRemoteView.setTextViewText(R.id.notification_text, content);
+        bigRemoteView.setTextViewText(R.id.notification_time, notifyTime);
+        //4.2以下版本
+        if (android.os.Build.VERSION.SDK_INT >= 16) {
+            Notification.Builder mBuilder = new Notification.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(title)
+                    .setContentText(content)
+                    .setContentIntent(pendingIntent)
+                    .setStyle(new Notification.BigTextStyle().bigText(content)
+                            .setBigContentTitle(title))
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setTicker(title)
+                    .setWhen(System.currentTimeMillis());
+            nManager.notify(0, mBuilder.build());
+        } else {
+            NotificationCompat.Builder ncBuilder = new NotificationCompat.Builder(context)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentTitle(title)
+                    .setContentText(content)
+                    .setContentIntent(pendingIntent)
+                    .setTicker(title)
+                    .setStyle(new NotificationCompat.BigTextStyle().bigText(content).setBigContentTitle(title))
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setPriority(NotificationCompat.PRIORITY_MAX)
+                    .setWhen(System.currentTimeMillis());
+            nManager.notify((int) System.currentTimeMillis(), ncBuilder.build());
         }
     }
 }
