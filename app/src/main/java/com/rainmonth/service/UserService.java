7051f00db06099afe177f87af06883ccae4f5962
Randy <rainmonth@yeah.net>
2016-09-20 11:29:26 +0800

    update UserService.java

--- app/src/main/java/com/rainmonth/service/UserService.java
+++ app/src/main/java/com/rainmonth/service/UserService.java
@@ -4,12 +4,14 @@ import com.rainmonth.bean.UserInfo;
 import com.rainmonth.utils.http.UserLoginResponse;
 
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by RandyZhang on 16/9/19.
@@ -24,4 +26,15 @@ public interface UserService {
 
     @GET("User/getUserInfo")
     Call<UserInfo> getUserInfo(@Query("id") int id);
+
+    @FormUrlEncoded
+    @POST("User/login")
+    Observable<Response<UserLoginResponse>> loginRx(@Field("username") String username, @Field("psw") String psw);
+
+    @FormUrlEncoded
+    @POST("User/Register")
+    Observable<Response<UserLoginResponse>> registerRx(@Body UserInfo userInfo);
+
+    @GET("User/getUserInfo")
+    Observable<Response<UserLoginResponse>> getUserInfoRx(@Query("id") int id);
 }
