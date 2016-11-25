9e525e56cf668d78a9b7184102cfff441c696b3a
Randy <rainmonth@yeah.net>
2016-11-25 16:07:11 +0800

    添加magicindicator库

---
+++ magicindicator/src/main/java/net/lucode/hackware/magicindicator/FragmentContainerHelper.java
@@ -0,0 +1,80 @@
+package net.lucode.hackware.magicindicator;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.view.animation.Interpolator;
+
+
+/**
+ * 使得MagicIndicator在FragmentContainer中使用
+ * <p>
+ * Created by hackware on 2016/9/4.
+ */
+
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class FragmentContainerHelper extends AnimatorListenerAdapter implements ValueAnimator.AnimatorUpdateListener {
+    private MagicIndicator mMagicIndicator;
+    private ValueAnimator mScrollAnimator;
+    private int mDuration = 200;
+    private Interpolator mInterpolator;
+
+    public FragmentContainerHelper(MagicIndicator magicIndicator) {
+        mMagicIndicator = magicIndicator;
+    }
+
+    public void handlePageSelected(int selectedIndex) {
+        if (mScrollAnimator == null || !mScrollAnimator.isRunning()) {
+            dispatchPageScrollStateChanged(ScrollState.SCROLL_STATE_SETTLING);
+        }
+        dispatchPageSelected(selectedIndex);
+        float currentPositionOffsetSum = 0.0f; // position = 0, positionOffset = 0.0f
+        if (mScrollAnimator != null) {
+            currentPositionOffsetSum = (Float) mScrollAnimator.getAnimatedValue();
+            mScrollAnimator.cancel();
+            mScrollAnimator = null;
+        }
+        mScrollAnimator = new ValueAnimator();
+        mScrollAnimator.setFloatValues(currentPositionOffsetSum, selectedIndex);    // position = selectedIndex, positionOffset = 0.0f
+        mScrollAnimator.addUpdateListener(this);
+        mScrollAnimator.addListener(this);
+        mScrollAnimator.setInterpolator(mInterpolator);
+        mScrollAnimator.setDuration(mDuration);
+        mScrollAnimator.start();
+    }
+
+    public void setDuration(int duration) {
+        mDuration = duration;
+    }
+
+    public void setInterpolator(Interpolator interpolator) {
+        mInterpolator = interpolator;
+    }
+
+    private void dispatchPageSelected(int pageIndex) {
+        mMagicIndicator.onPageSelected(pageIndex);
+    }
+
+    private void dispatchPageScrollStateChanged(int state) {
+        mMagicIndicator.onPageScrollStateChanged(state);
+    }
+
+    private void dispatchPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        mMagicIndicator.onPageScrolled(position, positionOffset, positionOffsetPixels);
+    }
+
+    @Override
+    public void onAnimationUpdate(ValueAnimator animation) {
+        float positionOffsetSum = (Float) animation.getAnimatedValue();
+        int position = (int) positionOffsetSum;
+        float positionOffset = positionOffsetSum - position;
+        dispatchPageScrolled(position, positionOffset, 0);
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+        mMagicIndicator.onPageScrollStateChanged(ScrollState.SCROLL_STATE_IDLE);
+    }
+}
