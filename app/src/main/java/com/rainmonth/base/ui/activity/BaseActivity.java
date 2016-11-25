4706d63452fac491370cd550c83fea5724c95db2
Randy <rainmonth@yeah.net>
2016-11-25 15:21:43 +0800

    app 项目 BaseActivity.java精简

--- app/src/main/java/com/rainmonth/base/ui/activity/BaseActivity.java
+++ app/src/main/java/com/rainmonth/base/ui/activity/BaseActivity.java
@@ -1,114 +1,32 @@
 package com.rainmonth.base.ui.activity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
 
 import com.rainmonth.R;
+import com.rainmonth.library.base.BaseAppCompatActivity;
 
 import butterknife.ButterKnife;
 
 /**
  * Activity 基类
  */
-public abstract class BaseActivity extends AppCompatActivity {
-
-    protected Context mContext;
-    protected String Tag = "";
-    protected Toolbar mToolbar;
+public abstract class BaseActivity extends BaseAppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getContentViewLayoutId() != 0) {
-            setContentView(getContentViewLayoutId());
-            // 以代码的形式为每个Activity对应的布局文件添加fitsSystemWindows属性
-            ViewGroup contentFrameLayout = (ViewGroup) findViewById(Window.ID_ANDROID_CONTENT);
-            assert contentFrameLayout != null;
-            View parentView = contentFrameLayout.getChildAt(0);
-            if (parentView != null && Build.VERSION.SDK_INT >= 14) {
-                parentView.setFitsSystemWindows(true);
-            }
-        } else {
-            throw new IllegalArgumentException("You must return a right contentView layout resource Id");
-        }
-        ButterKnife.bind(this);
+
         mToolbar = ButterKnife.findById(this, R.id.toolbar);
         if (null != mToolbar) {
             initToolbar();
         }
-        mContext = this;
-        Tag = this.getClass().getSimpleName();
-        initViewsAndEvent();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        ButterKnife.unbind(this);
     }
-
-    public abstract boolean isApplyTranslucentStatusBar();
-
-    public abstract int getContentViewLayoutId();
-
-    public abstract void initViewsAndEvent();
 
     public abstract void initToolbar();
 
-    /**
-     * startActivity
-     *
-     * @param clazz class to start
-     */
-    protected void readyGo(Class<?> clazz) {
-        Intent intent = new Intent(mContext, clazz);
-        startActivity(intent);
-    }
-
-    /**
-     * startActivity with bundle
-     *
-     * @param clazz  class to start
-     * @param bundle bundle to transport
-     */
-    protected void readyGo(Class<?> clazz, Bundle bundle) {
-        Intent intent = new Intent(mContext, clazz);
-        if (null != bundle) {
-            intent.putExtras(bundle);
-        }
-        startActivity(intent);
-    }
-
-    /**
-     * startActivityForResult
-     *
-     * @param clazz       class to start
-     * @param requestCode request code
-     */
-    protected void readyGoForResult(Class<?> clazz, int requestCode) {
-        Intent intent = new Intent(mContext, clazz);
-        startActivityForResult(intent, requestCode);
-    }
-
-    /**
-     * startActivityForResult with bundle
-     *
-     * @param clazz       class to start
-     * @param requestCode request code
-     * @param bundle      bundle to transport
-     */
-    protected void readyGoForResult(Class<?> clazz, int requestCode, Bundle bundle) {
-        Intent intent = new Intent(mContext, clazz);
-        if (null != bundle) {
-            intent.putExtras(bundle);
-        }
-        startActivityForResult(intent, requestCode);
-    }
 }
