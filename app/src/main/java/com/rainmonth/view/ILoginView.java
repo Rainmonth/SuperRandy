7ad54949e1be1e5e4b5031ad4fa2a85fa4cd24f2
Randy <rainmonth@yeah.net>
2017-01-23 11:44:58 +0800

    update

--- app/src/main/java/com/rainmonth/view/ILoginView.java
+++ app/src/main/java/com/rainmonth/view/ILoginView.java
@@ -1,11 +1,11 @@
 package com.rainmonth.view;
 
+import com.rainmonth.base.mvp.BaseResponse;
 import com.rainmonth.base.mvp.BaseView;
-import com.rainmonth.utils.http.UserLoginResponse;
 
 /**
  * Created by RandyZhang on 16/9/20.
  */
 public interface ILoginView extends BaseView {
-    void naveToAfterLogin(UserLoginResponse response);
+    void naveToAfterLogin(BaseResponse response);
 }
--- app/src/main/java/com/rainmonth/view/IRegisterView.java
+++ app/src/main/java/com/rainmonth/view/IRegisterView.java
@@ -1,11 +1,11 @@
 package com.rainmonth.view;
 
+import com.rainmonth.base.mvp.BaseResponse;
 import com.rainmonth.base.mvp.BaseView;
-import com.rainmonth.utils.http.UserLoginResponse;
 
 /**
  * Created by RandyZhang on 16/9/20.
  */
 public interface IRegisterView extends BaseView {
-    void naveToAfterRegister(UserLoginResponse response);
+    void naveToAfterRegister(BaseResponse response);
 }
