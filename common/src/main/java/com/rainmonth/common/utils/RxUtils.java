71fd31dcb61333d3769d9384b460bc6a02dfbd43
Randy <rainmonth@yeah.net>
2018-05-18 15:45:12 +0800

    add->公共库新增RxUtils工具类

---
+++ common/src/main/java/com/rainmonth/common/utils/RxUtils.java
@@ -0,0 +1,24 @@
+package com.rainmonth.common.utils;
+
+import org.reactivestreams.Publisher;
+
+import io.reactivex.Flowable;
+import io.reactivex.FlowableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Created by RandyZhang on 2018/5/17.
+ */
+
+public class RxUtils {
+    public static <T> FlowableTransformer<T, T> getFlowableTransformer() {
+        return new FlowableTransformer<T, T>() {
+            @Override
+            public Publisher<T> apply(Flowable<T> upstream) {
+                return upstream.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread());
+            }
+        };
+    }
+}
