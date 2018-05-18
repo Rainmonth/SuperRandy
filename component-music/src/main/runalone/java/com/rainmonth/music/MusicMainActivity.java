4bba1f62167cae5ee4ace4aae61ee9a609410ea9
Randy <rainmonth@yeah.net>
2018-05-18 15:46:12 +0800

    add->annotationProcessor增加Glide处理

--- component-music/src/main/runalone/java/com/rainmonth/music/MusicMainActivity.java
+++ component-music/src/main/runalone/java/com/rainmonth/music/MusicMainActivity.java
@@ -8,6 +8,6 @@ public class MusicMainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_music_main);
+        setContentView(R.layout.music_activity_main);
     }
 }
