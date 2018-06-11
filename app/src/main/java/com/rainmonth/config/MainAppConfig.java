97666fb699dac4a2dfb36bd67a94b1e03af791f8
Randy <rainmonth@yeah.net>
2018-06-11 16:46:26 +0800

    add->Main Project->主Module提供数据服务注入

---
+++ app/src/main/java/com/rainmonth/config/MainAppConfig.java
@@ -0,0 +1,38 @@
+package com.rainmonth.config;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.rainmonth.api.BannerService;
+import com.rainmonth.common.base.BaseApplicationDelegate;
+import com.rainmonth.common.di.module.GlobeConfigModule;
+import com.rainmonth.common.integration.ConfigModule;
+import com.rainmonth.common.integration.IRepositoryManager;
+
+import java.util.List;
+
+/**
+ * 该类用来声明数据请求服务、数据库服务等的配置，需要用
+ * Created by RandyZhang on 2018/6/11.
+ */
+public class MainAppConfig implements ConfigModule {
+    @Override
+    public void applyOptions(Context context, GlobeConfigModule.Builder builder) {
+
+    }
+
+    @Override
+    public void registerComponents(Context context, IRepositoryManager repositoryManager) {
+        repositoryManager.injectRetrofitService(BannerService.class);
+    }
+
+    @Override
+    public void injectAppLifecycle(Context context, List<BaseApplicationDelegate.Lifecycle> lifecycleList) {
+
+    }
+
+    @Override
+    public void injectActivityLifecycle(Context context, List<Application.ActivityLifecycleCallbacks> lifecycleList) {
+
+    }
+}
