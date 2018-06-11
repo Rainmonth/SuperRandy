8749b717ccfd202dacef27a8927c50c8fe137989
Randy <rainmonth@yeah.net>
2018-06-11 16:41:17 +0800

    add->Common Project->数据请求基类（提供基本字段），具体业务请求响应需继承该类

---
+++ common/src/main/java/com/rainmonth/common/base/BaseResponse.java
@@ -0,0 +1,42 @@
+package com.rainmonth.common.base;
+
+/**
+ * 基本网络返回数据
+ * Created by RandyZhang on 2018/6/11.
+ */
+public class BaseResponse {
+
+    /**
+     * success : true
+     * message : 请求成功
+     * code : 1
+     */
+
+    private boolean success;
+    private String message;
+    private int code;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
