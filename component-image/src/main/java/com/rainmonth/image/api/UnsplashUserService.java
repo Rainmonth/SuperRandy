68c3408dc0f30f5b6711879a7a720ad78f572471
Randy <rainmonth@yeah.net>
2018-08-10 00:34:12 +0800

    mod->image component->api 带路径的写法

--- component-image/src/main/java/com/rainmonth/image/api/UnsplashUserService.java
+++ component-image/src/main/java/com/rainmonth/image/api/UnsplashUserService.java
@@ -6,6 +6,7 @@ import io.reactivex.Flowable;
 import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
@@ -19,10 +20,10 @@ public interface UnsplashUserService {
             "Accept-Version:v1",
             "Authorization:Client-ID ae1715b58d53e958f990d42c9a3e221120a292efd592d66d0ba3717ccc4c9abe"
     })
-    @GET("https://api.unsplash.com/users/:username")
-    Flowable<Response<BaseResponse>> getUserInfo(@Query("w") int w,
-                                                 @Query("h") int h,
-                                                 @Query("username") String username);
+    @GET("https://api.unsplash.com/users/{username}")
+    Flowable<Response<BaseResponse>> getUserInfo(@Path("username") String username,
+                                                 @Query("w") int w,
+                                                 @Query("h") int h);
 
 
 }
