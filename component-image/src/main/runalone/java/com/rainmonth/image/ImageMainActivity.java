4f9722b35742fe9681a69af4a3b6896196af2a76
Randy <rainmonth@yeah.net>
2018-08-10 00:35:40 +0800

    mod->image component->测试跳转

--- component-image/src/main/runalone/java/com/rainmonth/image/ImageMainActivity.java
+++ component-image/src/main/runalone/java/com/rainmonth/image/ImageMainActivity.java
@@ -1,13 +1,35 @@
 package com.rainmonth.image;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
 
-public class ImageMainActivity extends AppCompatActivity {
+import com.rainmonth.common.base.BaseActivity;
+import com.rainmonth.common.di.component.AppComponent;
+import com.rainmonth.image.mvp.ui.activity.ImageHomeActivity;
+
+public class ImageMainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.image_activity_main);
+    protected int getContentViewLayoutID() {
+        return R.layout.image_activity_main;
+    }
+
+    @Override
+    protected void initViewsAndEvents() {
+        findViewById(R.id.tv_welcom).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readyGo(ImageHomeActivity.class);
+            }
+        });
+    }
+
+    @Override
+    protected void setupActivityComponent(AppComponent appComponent) {
+
+    }
+
+    @Override
+    public void initToolbar(int colorResId) {
+
     }
 }
