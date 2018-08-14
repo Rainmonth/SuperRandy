23422ccad77702c2b7b99bf2a9872d479fd71a68
Randy <rainmonth@yeah.net>
2018-08-15 00:25:23 +0800

    mod->image-component->更新入口Activity

--- component-image/src/main/runalone/java/com/rainmonth/image/ImageMainActivity.java
+++ component-image/src/main/runalone/java/com/rainmonth/image/ImageMainActivity.java
@@ -1,38 +1,27 @@
 package com.rainmonth.image;
 
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
 
 import com.rainmonth.common.base.BaseActivity;
 import com.rainmonth.common.di.component.AppComponent;
-import com.rainmonth.image.di.component.DaggerImageHomeComponent;
-import com.rainmonth.image.di.module.ImageHomeModule;
-import com.rainmonth.image.di.module.UnsplashUserModule;
-import com.rainmonth.image.mvp.contract.ImageHomeContract;
-import com.rainmonth.image.mvp.contract.UnsplashUserContract;
-import com.rainmonth.image.mvp.model.bean.PhotoBean;
-import com.rainmonth.image.mvp.model.bean.UserBean;
-import com.rainmonth.image.mvp.presenter.ImageHomePresenter;
-import com.rainmonth.image.mvp.ui.adapter.PhotosAdapter;
+import com.rainmonth.image.mvp.ui.activity.CollectionHomeActivity;
+import com.rainmonth.image.mvp.ui.activity.PhotoHomeActivity;
+import com.rainmonth.image.mvp.ui.activity.SearchActivity;
 
-import java.util.List;
+import butterknife.BindView;
 
-public class ImageMainActivity extends BaseActivity<ImageHomePresenter>
-        implements ImageHomeContract.View, UnsplashUserContract.View {
+public class ImageMainActivity extends BaseActivity {
 
-    SwipeRefreshLayout srfContainer;
-    RecyclerView rvPhotos;
-    PhotosAdapter photosAdapter;
+    @BindView(R.id.image_btn_photo)
+    Button imageBtnPhoto;
+    @BindView(R.id.image_btn_collection)
+    Button imageBtnCollection;
+    @BindView(R.id.image_btn_search)
+    Button imageBtnSearch;
 
     @Override
     protected void setupActivityComponent(AppComponent appComponent) {
-        DaggerImageHomeComponent.builder()
-                .appComponent(appComponent)
-                .imageHomeModule(new ImageHomeModule(this))
-                .unsplashUserModule(new UnsplashUserModule(this))
-                .build()
-                .inject(this);
+
     }
 
     @Override
@@ -42,32 +31,13 @@ public class ImageMainActivity extends BaseActivity<ImageHomePresenter>
 
     @Override
     protected void initViewsAndEvents() {
-        try {
-            srfContainer = findViewById(R.id.image_srl_container);
-            rvPhotos = findViewById(R.id.image_rv_photos);
-//
-            photosAdapter = new PhotosAdapter(mContext, R.layout.image_rv_item_photos);
-            rvPhotos.setAdapter(photosAdapter);
-            LinearLayoutManager manager = new LinearLayoutManager(mContext);
-            rvPhotos.setLayoutManager(manager);
-            mPresenter.getPhotos(1, 10, "latest");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        imageBtnPhoto.setOnClickListener(v -> readyGo(PhotoHomeActivity.class));
+        imageBtnCollection.setOnClickListener(v -> readyGo(CollectionHomeActivity.class));
+        imageBtnSearch.setOnClickListener(v -> readyGo(SearchActivity.class));
     }
 
     @Override
     public void initToolbar(int colorResId) {
-
-    }
-
-    @Override
-    public void initPhotoList(List<PhotoBean> photoBeans) {
-        photosAdapter.addData(photoBeans);
-    }
-
-    @Override
-    public void initUserInfo(UserBean responseResponse) {
 
     }
 }
