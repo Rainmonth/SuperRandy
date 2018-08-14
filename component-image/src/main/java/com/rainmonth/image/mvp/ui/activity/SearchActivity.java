06e6236326090bdd4ded44c78b970ae4bf60c2c0
Randy <rainmonth@yeah.net>
2018-08-15 00:29:06 +0800

    add->image-component->添加SearchActivity入口

---
+++ component-image/src/main/java/com/rainmonth/image/mvp/ui/activity/SearchActivity.java
@@ -0,0 +1,30 @@
+package com.rainmonth.image.mvp.ui.activity;
+
+import com.rainmonth.common.base.BaseActivity;
+import com.rainmonth.common.di.component.AppComponent;
+import com.rainmonth.image.R;
+
+/**
+ * Created by RandyZhang on 2018/8/14.
+ */
+public class SearchActivity extends BaseActivity {
+    @Override
+    protected void setupActivityComponent(AppComponent appComponent) {
+
+    }
+
+    @Override
+    public void initToolbar(int colorResId) {
+
+    }
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.image_activity_search;
+    }
+
+    @Override
+    protected void initViewsAndEvents() {
+
+    }
+}
