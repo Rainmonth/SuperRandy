db5ca43e4d8c543332ac58e0ccea329fd5cb96ae
张豪成 <zhanghaocheng@hhdd.com>
2018-08-23 14:43:55 +0800

    mod->App component->app添加入口

--- component-app/src/main/runalone/java/com/rainmonth/app/AppMainActivity.java
+++ component-app/src/main/runalone/java/com/rainmonth/app/AppMainActivity.java
@@ -2,6 +2,10 @@ package com.rainmonth.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.rainmonth.router.RouterConstant;
+import com.rainmonth.router.RouterUtils;
 
 public class AppMainActivity extends AppCompatActivity {
 
@@ -9,5 +13,12 @@ public class AppMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.app_activity_main);
+
+        findViewById(R.id.app_btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RouterUtils.getInstance().build(RouterConstant.PATH_APP_HOME).navigation();
+            }
+        });
     }
 }
