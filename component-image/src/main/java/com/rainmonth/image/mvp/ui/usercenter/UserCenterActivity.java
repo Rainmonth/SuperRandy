9434d913662cbc4a8e4a6ac75bdf258166929952
张豪成 <zhanghaocheng@hhdd.com>
2018-09-30 15:33:25 +0800

    mod、add->component-image->用户中心页面跳转完成

--- component-image/src/main/java/com/rainmonth/image/mvp/ui/usercenter/UserCenterActivity.java
+++ component-image/src/main/java/com/rainmonth/image/mvp/ui/usercenter/UserCenterActivity.java
@@ -1,6 +1,11 @@
 package com.rainmonth.image.mvp.ui.usercenter;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.rainmonth.common.base.BaseActivity;
+import com.rainmonth.common.base.BaseWebActivity;
 import com.rainmonth.common.di.component.AppComponent;
 import com.rainmonth.image.R;
 
@@ -31,6 +36,64 @@ public class UserCenterActivity extends BaseActivity {
 
     @Override
     protected void initViewsAndEvents() {
-
+        Button downloadBtn = findViewById(R.id.downloadBtn);
+        downloadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readyGo(DownloadCenterActivity.class);
+            }
+        });
+        Button collectBtn = findViewById(R.id.collectBtn);
+        collectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readyGo(UserImageCollectionActivity.class);
+            }
+        });
+        Button focusBtn = findViewById(R.id.focusBtn);
+        focusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readyGo(UserFocusActivity.class);
+            }
+        });
+        Button infoBtn = findViewById(R.id.infoBtn);
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readyGo(UserInfoActivity.class);
+            }
+        });
+        Button statisticsBtn = findViewById(R.id.statisticsBtn);
+        statisticsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readyGo(UserStatisticsActivity.class);
+            }
+        });
+        Button siteBtn = findViewById(R.id.siteBtn);
+        siteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString(BaseWebActivity.BUNDLE_KEY_URL, "http://rainmonth.cn");
+                bundle.putString(BaseWebActivity.BUNDLE_KEY_TITLE, "荏苒追寻");
+                readyGo(BaseWebActivity.class, bundle);
+            }
+        });
+        Button clearCacheBtn = findViewById(R.id.clearCacheBtn);
+        clearCacheBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("清除缓存");
+            }
+        });
+        Button showSettingBtn = findViewById(R.id.showSettingBtn);
+        showSettingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readyGo(SettingImageShowStyleActivity.class);
+            }
+        });
     }
 }
---
+++ component-image/src/main/java/com/rainmonth/image/mvp/ui/usercenter/UserStatisticsActivity.java
@@ -0,0 +1,33 @@
+package com.rainmonth.image.mvp.ui.usercenter;
+
+import com.rainmonth.common.base.BaseActivity;
+import com.rainmonth.common.di.component.AppComponent;
+import com.rainmonth.image.R;
+
+/**
+ * @date: 2018/9/30
+ * @author: randy
+ * @description: 用户统计页面
+ */
+public class UserStatisticsActivity extends BaseActivity {
+
+    @Override
+    protected int getContentViewLayoutID() {
+        return R.layout.image_activity_user_statistics;
+    }
+
+    @Override
+    protected void initViewsAndEvents() {
+
+    }
+
+    @Override
+    protected void setupActivityComponent(AppComponent appComponent) {
+
+    }
+
+    @Override
+    public void initToolbar(int colorResId) {
+
+    }
+}
