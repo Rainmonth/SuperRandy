db54852bc0ef1c2b606e15a6c45724f9ffba5383
Randy <rainmonth@yeah.net>
2018-10-06 10:02:30 +0800

    mod->component-image->合集详情接口调通

--- component-image/src/main/java/com/rainmonth/image/mvp/ui/collection/CollectionDetailActivity.java
+++ component-image/src/main/java/com/rainmonth/image/mvp/ui/collection/CollectionDetailActivity.java
@@ -1,7 +1,13 @@
 package com.rainmonth.image.mvp.ui.collection;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.view.View;
 
+import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.rainmonth.common.base.BaseActivity;
 import com.rainmonth.common.di.component.AppComponent;
 import com.rainmonth.image.R;
@@ -11,7 +17,8 @@ import com.rainmonth.image.di.module.CollectionDetailMoudle;
 import com.rainmonth.image.mvp.contract.CollectionDetailContract;
 import com.rainmonth.image.mvp.model.bean.PhotoBean;
 import com.rainmonth.image.mvp.presenter.CollectionDetailPresenter;
-import com.socks.library.KLog;
+import com.rainmonth.image.mvp.ui.adapter.PhotosAdapter;
+import com.rainmonth.image.mvp.ui.photo.PhotoDetailActivity;
 
 import java.util.List;
 
@@ -25,8 +32,12 @@ import java.util.List;
  */
 public class CollectionDetailActivity extends BaseActivity<CollectionDetailPresenter>
         implements CollectionDetailContract.View {
+    private SwipeRefreshLayout detailSrl;
+    private RecyclerView detailPhotosRv;
+    PhotosAdapter photosAdapter;
     private long collectionId;
     int page = 1, perPage = 10;
+    private boolean isRefresh = false;
 
     @Override
     protected int getContentViewLayoutID() {
@@ -35,6 +46,63 @@ public class CollectionDetailActivity extends BaseActivity<CollectionDetailPrese
 
     @Override
     protected void initViewsAndEvents() {
+        detailSrl = findViewById(R.id.collectionDetailSrl);
+        detailPhotosRv = findViewById(R.id.collectionDetailPhotosRv);
+        detailSrl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                doRefresh();
+            }
+        });
+
+        photosAdapter = new PhotosAdapter(mContext, R.layout.image_rv_item_photos);
+        photosAdapter.setEnableLoadMore(true);
+        photosAdapter.setOnLoadMoreListener(new BaseQuickAdapter.RequestLoadMoreListener() {
+            @Override
+            public void onLoadMoreRequested() {
+                isRefresh = false;
+                mPresenter.getCollectionPhotos(collectionId, page, perPage);
+            }
+        }, detailPhotosRv);
+
+        photosAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+                // 一次传递一页数据，并传递当前索引及页面，方便数据请求
+                int currentPage = position / perPage + 1;
+                int currentIndex = position % perPage;
+                Bundle bundle = new Bundle();
+                List<PhotoBean> currentPagePhotos = photosAdapter.getData()
+                        .subList((currentPage - 1) * perPage, currentPage * perPage);
+                SparseArray<PhotoBean> beanSparseArray =
+                        convertListToSparseArray(currentPagePhotos);
+                bundle.putSparseParcelableArray(Consts.PHOTO_LIST, beanSparseArray);
+                bundle.putInt(Consts.CURRENT_PAGE, page);
+                bundle.putInt(Consts.CURRENT_INDEX, currentIndex);
+                bundle.putInt(Consts.PAGE_SIZE, perPage);
+                readyGo(PhotoDetailActivity.class, bundle);
+            }
+        });
+
+        detailPhotosRv.setAdapter(photosAdapter);
+        GridLayoutManager manager = new GridLayoutManager(mContext, 2);
+        detailPhotosRv.setLayoutManager(manager);
+        doRefresh();
+    }
+
+    private SparseArray<PhotoBean> convertListToSparseArray(List<PhotoBean> photoBeans) {
+        SparseArray<PhotoBean> beanSparseArray = new SparseArray<>();
+        for (int i = 0; i < photoBeans.size(); i++) {
+            beanSparseArray.append(i, photoBeans.get(i));
+        }
+
+        return beanSparseArray;
+    }
+
+    private void doRefresh() {
+        isRefresh = true;
+        detailSrl.setRefreshing(true);
+        page = 1;
         mPresenter.getCollectionPhotos(collectionId, page, perPage);
     }
 
@@ -64,6 +132,21 @@ public class CollectionDetailActivity extends BaseActivity<CollectionDetailPrese
 
     @Override
     public void initCollectionPhotoList(List<PhotoBean> photoBeans) {
-        KLog.d("Randy", photoBeans);
+        detailSrl.setRefreshing(false);
+        final int size = photoBeans.size();
+        if (isRefresh) {
+            photosAdapter.setNewData(photoBeans);
+            isRefresh = false;
+        } else {
+            if (size > 0) {
+                photosAdapter.addData(photoBeans);
+            }
+        }
+        if (perPage == photoBeans.size()) {
+            photosAdapter.loadMoreComplete();
+        } else if (page > photoBeans.size()) {
+            photosAdapter.loadMoreEnd(true);
+        }
+        page++;
     }
 }
