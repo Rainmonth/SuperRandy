ad1fa30c9cd6169bb1e38428861f9f32555d3c50
张豪成 <zhanghaocheng@hhdd.com>
2019-10-30 21:02:10 +0800

    exoplayer temp

--- component-music/src/main/java/com/rainmonth/music/videoplayer/VideoPlayerActivity.java
+++ component-music/src/main/java/com/rainmonth/music/videoplayer/VideoPlayerActivity.java
@@ -1,9 +1,23 @@
 package com.rainmonth.music.videoplayer;
 
+import androidx.annotation.Nullable;
+
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlaybackException;
 import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.PlaybackPreparer;
 import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.RenderersFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.RandomTrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.ui.PlayerControlView;
 import com.google.android.exoplayer2.ui.PlayerView;
 import com.google.android.exoplayer2.util.EventLogger;
@@ -13,13 +27,25 @@ import com.rainmonth.music.R;
 import com.socks.library.KLog;
 
 /**
+ * 计划提供以下示例：
+ * 1、列表焦点播放；
+ * 2、悬浮窗播放
+ * 3、抖音式播放
+ * 4、列表式播放
+ * 5、支持播放视图的比例设置
+ *
  * @date: 2018-12-20
  * @author: randy
  * @description: 视频播放器
  */
-public class VideoPlayerActivity extends BaseActivity implements PlayerControlView.VisibilityListener {
-    Player player;
+public class VideoPlayerActivity extends BaseActivity implements PlayerControlView.VisibilityListener,
+        PlaybackPreparer {
+    SimpleExoPlayer player;
     PlayerView playerView;
+
+    DefaultTrackSelector trackSelector;
+    boolean preferExtensionDecoder = false;
+    boolean startAutoPlay = true;
 
     @Override
     protected void setupActivityComponent(AppComponent appComponent) {
@@ -51,13 +77,40 @@ public class VideoPlayerActivity extends BaseActivity implements PlayerControlVi
     }
 
     private void initPlayer() {
-//        player = ExoPlayerFactory.newSimpleInstance(
-//                        /* context= */ this, renderersFactory, trackSelector, drmSessionManager);
-//        player.addListener(new PlayerEventListener());
-//        player.setPlayWhenReady(startAutoPlay);
-//        player.addAnalyticsListener(new EventLogger(trackSelector));
-//        playerView.setPlayer(player);
-//        playerView.setPlaybackPreparer(this);
+        if (player == null) {
+            TrackSelection.Factory trackSelectionFactory = new RandomTrackSelection.Factory();
+            trackSelector = new DefaultTrackSelector(trackSelectionFactory);
+//            TrackSelection.Factory trackSelectionFactory = new AdaptiveTrackSelection.Factory();
+//            String abrAlgorithm = intent.getStringExtra(ABR_ALGORITHM_EXTRA);
+//            if (abrAlgorithm == null || ABR_ALGORITHM_DEFAULT.equals(abrAlgorithm)) {
+//                trackSelectionFactory = new AdaptiveTrackSelection.Factory();
+//            } else if (ABR_ALGORITHM_RANDOM.equals(abrAlgorithm)) {
+//                trackSelectionFactory = new RandomTrackSelection.Factory();
+//            } else {
+//                showToast(R.string.error_unrecognized_abr_algorithm);
+//                finish();
+//                return;
+//            }
+            RenderersFactory renderersFactory =
+                    buildRenderersFactory(preferExtensionDecoder);
+            player = ExoPlayerFactory.newSimpleInstance(
+                    /* context= */ this, renderersFactory, trackSelector);
+            player.addListener(new PlayerEventListener());
+            player.setPlayWhenReady(startAutoPlay);
+            player.addAnalyticsListener(new EventLogger(trackSelector));
+            playerView.setPlayer(player);
+            playerView.setPlaybackPreparer(this);
+        }
+    }
+
+    private RenderersFactory buildRenderersFactory(boolean preferExtensionDecoders) {
+        @DefaultRenderersFactory.ExtensionRendererMode
+        int extensionRendererMode = preferExtensionDecoders
+                ? DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER
+                : DefaultRenderersFactory.EXTENSION_RENDERER_MODE_ON;
+
+        return new DefaultRenderersFactory(/* context= */ this)
+                .setExtensionRendererMode(extensionRendererMode);
     }
 
     @Override
@@ -87,5 +140,75 @@ public class VideoPlayerActivity extends BaseActivity implements PlayerControlVi
         }
 
         return null;
+    }
+
+    @Override
+    public void preparePlayback() {
+        KLog.d("preparePlayback");
+    }
+
+    /**
+     * 播放器事件监听器
+     */
+    public class PlayerEventListener implements Player.EventListener {
+        @Override
+        public void onTimelineChanged(Timeline timeline, @Nullable Object manifest, int reason) {
+            KLog.d("onTimelineChanged");
+        }
+
+        @Override
+        public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+            KLog.d("onTracksChanged");
+        }
+
+        @Override
+        public void onLoadingChanged(boolean isLoading) {
+            KLog.d("onLoadingChanged");
+        }
+
+        @Override
+        public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+            KLog.d("onPlayerStateChanged");
+        }
+
+        @Override
+        public void onPlaybackSuppressionReasonChanged(int playbackSuppressionReason) {
+            KLog.d("onPlaybackSuppressionReasonChanged");
+        }
+
+        @Override
+        public void onIsPlayingChanged(boolean isPlaying) {
+            KLog.d("onIsPlayingChanged");
+        }
+
+        @Override
+        public void onRepeatModeChanged(int repeatMode) {
+            KLog.d("onRepeatModeChanged");
+        }
+
+        @Override
+        public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+            KLog.d("onShuffleModeEnabledChanged");
+        }
+
+        @Override
+        public void onPlayerError(ExoPlaybackException error) {
+            KLog.d("onPlayerError");
+        }
+
+        @Override
+        public void onPositionDiscontinuity(int reason) {
+            KLog.d("onPositionDiscontinuity");
+        }
+
+        @Override
+        public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+            KLog.d("onPlaybackParametersChanged");
+        }
+
+        @Override
+        public void onSeekProcessed() {
+            KLog.d("onSeekProcessed");
+        }
     }
 }
