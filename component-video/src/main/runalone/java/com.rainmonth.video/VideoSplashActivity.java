cf3901865965de1b86a05b71e7189db2d3c0e838
张豪成 <zhanghaocheng@hhdd.com>
2019-11-05 13:12:44 +0800

    mod->重命名

--- component-video/src/main/runalone/java/com.rainmonth.video/VideoSplashActivity.java
+++ component-video/src/main/runalone/java/com.rainmonth.video/VideoSplashActivity.java
@@ -3,11 +3,17 @@ package com.rainmonth.video;
 import com.rainmonth.common.base.BaseActivity;
 import com.rainmonth.common.di.component.AppComponent;
 
-public class VideoLaunchActivity extends BaseActivity {
+/**
+ * 视频模块闪屏页
+ *
+ * @author 张豪成
+ * @date 2019/11/05
+ */
+public class VideoSplashActivity extends BaseActivity {
 
     @Override
     protected int getContentViewLayoutID() {
-        return R.layout.video_activity_video_launch;
+        return R.layout.video_activity_video_splash;
     }
 
     @Override
