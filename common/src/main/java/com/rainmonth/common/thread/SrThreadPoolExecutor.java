b591115ea7d62d0b30aa6f8e61343e26636316e9
Randy <rainmonth@yeah.net>
2020-09-24 18:34:19 +0800

    mod->调用start，否则拿到的Looper为null

--- common/src/main/java/com/rainmonth/common/thread/SrThreadPoolExecutor.java
+++ common/src/main/java/com/rainmonth/common/thread/SrThreadPoolExecutor.java
@@ -102,6 +102,7 @@ public class SrThreadPoolExecutor {
         mBufferQueue = getBufferQueue();
 
         mHandlerThread = new HandlerThread(name + "_HandlerThread");
+        mHandlerThread.start();
         mHandler = new Handler(mHandlerThread.getLooper(), mCallback);
     }
 
