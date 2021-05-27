80a9a99e0d3d05ed3733f7fb9c4e79bf4bfbc0f7
张豪成 <zhanghaocheng@hhdd.com>
2021-05-27 11:45:27 +0800

    update

---
+++ component-avsample/src/main/java/com/rainmonth/avsample/AvsampleMainActivity.java
@@ -0,0 +1,16 @@
+package com.rainmonth.avsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rainmonth.component_avsample.R;
+
+public class AvsampleMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.avsample_activity_main);
+    }
+}
